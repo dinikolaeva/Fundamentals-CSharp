9681f333a4352591bd18353bb3a52e975b0eeb24
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-01-23 14:29:43 +0200

    MoreExe

---
+++ 09.Arrays-MoreExercises/01. Encrypt,SortAndPrintArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks.Dataflow;
+
+namespace _001._Encrypt_SortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+
+            int[] sumOfCharArrays = new int[numberOfStrings];
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                int sum1 = 0;
+                int sum2 = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    int sumOfVowel = 0;
+                    int sumOfConsonant = 0;
+                    bool isVowel = "aeiouAEIOU".IndexOf(input[j]) >= 0;
+
+                    if (isVowel == true)
+                    {
+                        sumOfVowel += input[j];
+                    }
+                    else
+                    {
+                        sumOfConsonant += input[j];
+                    }
+
+                    sum1 = sum1 + sumOfVowel * input.Length;
+                    sum2 = sum2 + sumOfConsonant / input.Length;
+
+                }
+
+                int sumOfChars = sum1 + sum2;
+
+                sumOfCharArrays[i] = sumOfChars;
+            }
+
+            Array.Sort(sumOfCharArrays);
+
+            foreach (var elements in sumOfCharArrays)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
---
+++ 09.Arrays-MoreExercises/02.PascalTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Transactions;
+
+namespace _02.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = 1;
+
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($"{number} ");
+                    number = number * (i - j) / (j + 1);
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
