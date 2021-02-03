b9bcce53a30e13aff7efdfb1bac072e16ac623d7
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-03 09:05:38 +0200

    Exe

---
+++ 16.ObjectAndClasses-Exe/04.Students/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+
+                Student newStudent = new Student(input[0], input[1], double.Parse(input[2]));
+                students.Add(newStudent);
+            }
+
+            foreach (Student student in students.OrderByDescending(x => x.Grade))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+    }
+}
