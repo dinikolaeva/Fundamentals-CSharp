2ca3b9a12c71df66ed0be71fdafd97d9a9b9ef05
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-07 00:36:11 +0200

    Lab

---
+++ 17.AssociativeArrays-Lab/02.OddOccurrences/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            foreach (var currentWord in words)
+            {
+                string wordCase = currentWord.ToLower();
+
+                if (output.ContainsKey(wordCase))
+                {
+                    output[wordCase]++;
+                }
+                else
+                {
+                    output.Add(wordCase, 1);
+                }
+            }
+
+            foreach (var item in output)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write(item.Key + " ");
+                }
+            }
+        }
+    }
+}
