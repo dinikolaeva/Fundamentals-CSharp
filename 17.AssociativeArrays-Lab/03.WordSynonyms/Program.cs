f73457db5f88ebae02b255cc786f12f41c353cfb
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-08 00:13:26 +0200

    Lab

---
+++ 17.AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary <string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>() {synonyms});
+                }
+                else
+                {
+                    words[word].Add(synonyms);
+                }
+            }
+
+            foreach (var item in words)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
