1c13f571e7738d8b6f80b9cbed5a96554ee0887a
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-09 14:16:14 +0200

    Lab

---
+++ 17.AssociativeArrays-Lab/04.Largest3Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .Take(3)
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
