6fe6b05966e472826b85e2a1b3a7567df7b717ad
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-10 13:25:24 +0200

    Lab

---
+++ 17.AssociativeArrays-Lab/05.WordFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => n.Length % 2 == 0)
+                .ToArray();
+
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
