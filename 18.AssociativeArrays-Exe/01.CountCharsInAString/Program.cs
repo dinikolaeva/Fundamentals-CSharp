50ba0315665050ce17570411bcdc8f9a89b44fa1
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-11 13:58:08 +0200

    Exe

---
+++ 18.AssociativeArrays-Exe/01.CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine()
+                .ToCharArray();
+
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    continue;
+                }
+
+                if (!letters.ContainsKey(text[i]))
+                {
+                    letters.Add(text[i], 0);
+                }
+                letters[text[i]]++;
+            }
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
