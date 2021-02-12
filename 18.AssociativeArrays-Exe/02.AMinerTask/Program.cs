01a95115ddc6dda56958725ff597e86ca8fcbacc
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-12 13:42:36 +0200

    Exe

---
+++ 18.AssociativeArrays-Exe/02.AMinerTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resources = Console.ReadLine();
+
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+
+            while (resources != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!materials.ContainsKey(resources))
+                {
+                    materials.Add(resources, 0);
+                }
+                materials[resources] += quantity;
+
+                resources = Console.ReadLine();
+            }
+
+            foreach (var item in materials)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
