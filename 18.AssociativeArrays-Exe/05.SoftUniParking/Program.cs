11fde16d4b320ea1ae052e8791f3a73bde14e3e6
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-15 14:12:20 +0200

    Exe

---
+++ 18.AssociativeArrays-Exe/05.SoftUniParking/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> users = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = input[0];
+                string username = input[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = input[2];
+
+                    if (!users.ContainsKey(username))
+                    {
+                        users.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                }
+                else if (command== "unregister")
+                {
+                    if (users.ContainsKey(username))
+                    {
+                        users.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+    }
+}
