ce07526b7fdb97517e1f5287cef8bcee22c4bf19
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-21 19:18:22 +0200

    Exe

---
+++ 18.AssociativeArrays-Exe/10.SoftUniExamResults/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.SoftUniExamResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, List<string>> studentsResults = new Dictionary<string, List<string>>();
+
+            while (input != "exam finished")
+            {
+                string[] infoOfExam = input
+                    .Split('-')
+                    .ToArray();
+
+                string studentName = infoOfExam[0];
+                string language = infoOfExam[1];
+                int points = int.Parse(infoOfExam[2]);
+
+                if (!studentsResults.ContainsKey(studentName))
+                {
+                    studentsResults.Add(studentName, new List<string>());
+                    studentsResults[studentName].Add(language);
+                }
+
+                if (infoOfExam.Contains("banned"))
+                {
+                    studentsResults.Remove(studentName);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
