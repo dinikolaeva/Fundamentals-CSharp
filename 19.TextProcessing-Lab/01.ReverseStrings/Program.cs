bfd0ac16b88cfaba871aefab379a73df0134e56c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-22 09:42:28 +0200

    Lab

---
+++ 19.TextProcessing-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string reverse = string.Empty;
+
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    reverse += command[i];
+                }
+
+                Console.WriteLine($"{command} = {reverse}");
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
