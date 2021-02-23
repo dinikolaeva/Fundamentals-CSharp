4d8bbd6ef15b810cbd8e6b882fce7ecee044b608
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-23 13:01:38 +0200

    Lab

---
+++ 19.TextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in input)
+            {
+                int count = word.Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
