e5c9cdd364ec70dc88d141a81a8e2e6376be669a
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-24 13:29:16 +0200

    Lab

---
+++ 19.TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine().ToLower();
+            string secondString = Console.ReadLine();
+
+            int index = secondString.IndexOf(firstString);
+
+            while (index != -1)
+            {
+                secondString = secondString.Remove(index, firstString.Length);
+                index = secondString.IndexOf(firstString);
+            }
+
+            Console.WriteLine(secondString);
+        }
+    }
+}
