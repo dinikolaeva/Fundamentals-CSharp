125874f740b6127a4502cfe668eb0045adb00b80
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-25 09:39:14 +0200

    Lab

---
+++ 19.TextProcessing-Lab/04.TextFilter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            string replaceWord = string.Empty;
+
+            foreach (var word in banWords)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    replaceWord += "*";
+                }
+
+                text = text.Replace(word, replaceWord);
+                replaceWord = string.Empty;
+
+            }
+            
+            Console.WriteLine(text);
+        }
+    }
+}
