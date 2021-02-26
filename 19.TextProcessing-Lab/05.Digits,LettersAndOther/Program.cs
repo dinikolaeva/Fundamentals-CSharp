f3133814c00c2e212356882c1bc3c6b5d00c840b
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-26 09:22:04 +0200

    Lab

---
+++ 19.TextProcessing-Lab/05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _05.Digits_LettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+
+            StringBuilder sbDigits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder other = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (char.IsDigit(text[i]))
+                {
+                    sbDigits.Append(text[i]);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    letters.Append(text[i]);
+                }
+                else
+                {
+                    other.Append(text[i]);
+                }
+            }
+            Console.WriteLine(sbDigits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+
+        }
+
+        
+    }
+}
