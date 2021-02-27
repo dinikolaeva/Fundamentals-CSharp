b593267e7aee87fd58013a42e1cc687783e87a07
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-02-27 15:03:39 +0200

    Exe

---
+++ 20.TextProcessing-Exe/01.ValidUsernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (string user in userNames)
+            {
+                bool isValid = IsValidUsername(user);
+
+                if (isValid)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+
+        private static bool IsValidUsername(string user)
+        {
+
+            return user.Length > 3 && user.Length < 16
+                && user.All(x => char.IsLetterOrDigit(x)
+                || user.Contains('-')
+                || user.Contains('_'));
+          
+        }
+    }
+}
