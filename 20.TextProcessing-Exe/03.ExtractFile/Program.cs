ea3b7cf7e0ed71223b3772afbf0c6ffad36ca6a8
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-02 09:25:09 +0200

    Exe

---
+++ 20.TextProcessing-Exe/03.ExtractFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Console.ReadLine().Split(@"\");
+
+            var fileName = path[path.Length - 1];
+            var files = fileName.Split('.');
+
+            string name = files[0];
+            string extension = files[1];
+
+
+            Console.WriteLine($"File name: {name}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
