3ac76e2d50a66386e66d2acc36a2ef0129f5ce32
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-03 14:22:07 +0200

    Exe

---
+++ 20.TextProcessing-Exe/04.CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _4.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = (char)(input[i] + 3);
+                sb.Append(currentChar);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
