af1e1fd11f756964395230dd6e8f0c06851df239
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-04 11:49:54 +0200

    Exe

---
+++ 20.TextProcessing-Exe/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _5.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine().Trim('0');
+            int digit = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            int result = 0;
+            int rest = 0;
+
+            if (number == string.Empty || digit == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (var num in number.Reverse())
+            {
+                int multiplayer = int.Parse(num.ToString());
+
+                result = digit * multiplayer;
+
+                if (rest > 0)
+                {
+                    result += rest;
+                }
+
+                sb.Insert(0, result % 10);
+
+                rest = result / 10;
+
+            }
+
+            if (rest > 0)
+            {
+                sb.Insert(0, rest);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
