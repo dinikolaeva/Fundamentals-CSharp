daec244161b6f5b068f65016d7883954f435fa4b
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-05 09:34:19 +0200

    Exe

---
+++ 20.TextProcessing-Exe/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _6.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length-1; i++)
+            {
+                if (text[i] != text[i+1])
+                {
+                    sb.Append(text[i]);
+                }
+
+            }
+
+            char symbol = text[text.Length - 1];
+            sb.Append(symbol);
+
+            Console.WriteLine(sb.ToString());
+            
+        }
+    }
+}
