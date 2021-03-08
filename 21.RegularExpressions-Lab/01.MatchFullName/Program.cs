df3a1e0569e1bbd8d7698d22536cf93d3cca4bd8
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-08 11:36:01 +0200

    Lab

---
+++ 21.RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pattern = @"\b([A-Z]a?[a-z]+) ([A-Z][a-z]+)\b";
+
+            string text = Console.ReadLine();
+
+            MatchCollection mathcNames = Regex.Matches(text, pattern);
+
+            foreach (var name in mathcNames)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
