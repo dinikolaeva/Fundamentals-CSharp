3a585404998ec5dd8c57de226c33c8129806d66c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-09 10:15:38 +0200

    Lab

---
+++ 21.RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\+359([-| ])2(\1)([0-9]{3})(\1)([0-9]{4})\b";
+
+            var phones = Console.ReadLine();
+
+            var matches = Regex.Matches(phones,pattern);
+
+            var machedPhoneNumbers = matches
+                .Cast<Match>()
+                .Select(n=>n.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",machedPhoneNumbers));
+        }
+    }
+}
