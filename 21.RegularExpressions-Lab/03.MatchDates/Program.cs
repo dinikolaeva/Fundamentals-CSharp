d8d09dcf746ce7a8e343c290e43b697c2b7a8be5
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-10 09:38:00 +0200

    Lab

---
+++ 21.RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var input = Console.ReadLine();
+
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match date in matches)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
