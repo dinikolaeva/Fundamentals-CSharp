9b9d1bebd53c828199051c9e86a7e4c50fa16353
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-11 09:14:48 +0200

    Exe

---
+++ 22.RegularExpressions-Exe/01.Furniture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _1.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @">>(?<name>[A-z]+)<<(?<price>\d+\.?\d+)!(?<quantity>\d+)";
+
+            var input = Console.ReadLine();
+
+            var matchedItems = Regex.Matches(input, pattern);
+
+            double totalPrice = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+                foreach (Match item in matchedItems)
+                {
+                    var name = item.Groups["name"].Value;
+                    double price = double.Parse(item.Groups["price"].Value);
+                    int quantity = int.Parse(item.Groups["quantity"].Value);
+
+                    totalPrice += price * quantity;
+                    Console.WriteLine($"{name}");
+                }
+
+                input = Console.ReadLine();
+                matchedItems = Regex.Matches(input, pattern);
+            }
+
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+        }
+    }
+}
