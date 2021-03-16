eb711d7417899e1ac8510c19257e41a8362c064e
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-16 09:37:37 +0200

    Exe

---
+++ 22.RegularExpressions-Exe/06.ExtractEmails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _6.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+
+            string input = Console.ReadLine();
+
+            var validEmails = Regex.Matches(input, pattern);
+
+            foreach (Match email in validEmails)
+            {
+                Console.WriteLine($"{email}");
+            }
+        }
+    }
+}
