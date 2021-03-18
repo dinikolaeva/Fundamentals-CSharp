9a8e747cb401023e6e2d93c487a54e83116b70f1
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-18 11:12:28 +0200

    ExamPrep

---
+++ 23.FundamentalsMidExams2020/MidExams-Exersices/MidExam05July2020/01.SoftUniReception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            int neededHour = 0;
+
+            while (numberOfPeople > 0)
+            {
+                numberOfPeople -= (firstEmployee + secondEmployee + thirdEmployee);
+                neededHour++;
+
+                if (neededHour % 4 == 0)
+                {
+                    neededHour++;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {neededHour}h.");
+        }
+    }
+}
